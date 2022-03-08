9bd3945e90f2729d884ae629350e16acc6c4afbb
Kev Comedia <kevcomedia@gmail.com>
2022-03-08 18:54:56 +0800

    Add list styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      listStyleType: {
+        circle: 'circle',
+      },
+    },
   },
   plugins: [],
 }
